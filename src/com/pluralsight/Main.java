ef471cac5fdd1e9cd27116839720a4d3f91f5d8f
Kiki <kiki_e@hotmail.com>
2017-12-17 14:43:06 -0500

    Initial commit

---
+++ src/com/pluralsight/Main.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+public class Main {
+
+    /*
+https://programmingbydoing.com/
+
+Assignment #6 A letter to yourself
+*/
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("+----------------------------------------------------------------+");
+        System.out.println("|                                                          ####  |");
+        System.out.println("|                                                          ####  |");
+        System.out.println("|                                                          ####  |");
+        System.out.println("|                                                                |");
+        System.out.println("|                                        Bill Gates              |");
+        System.out.println("|                                        1 Microsoft Way         |");
+        System.out.println("|                                        Redmond, WA 98104       |");
+        System.out.println("|                                                                |");
+        System.out.println("+----------------------------------------------------------------+");
+
+    }
+}
